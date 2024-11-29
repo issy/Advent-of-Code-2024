45ea1e301a3b579372fc23a5fb2d313db21776c3
Issy Szemeti <issy.szemeti@starlingbank.com>
2024-11-29 16:06:22 +0000

    Add jetbrains annotations

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    compileOnly("org.jetbrains:annotations:26.0.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.26.3")
