82256b9fed25d426be8af70af2fefc8c3ce29100
issy <issy@issy.dev>
2024-11-29 22:56:59 +0000

    Add methods to Solution interface

--- src/main/java/com/adventofcode/solutions/Solution.java
+++ src/main/java/com/adventofcode/solutions/Solution.java
@@ -2,6 +2,10 @@ package com.adventofcode.solutions;
 
 public interface Solution {
 
+  String getInputFilename();
+
+  int getDayNumber();
+
   int solvePartOne(String input);
 
   int solvePartTwo(String input);
