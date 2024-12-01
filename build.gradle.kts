bc7b4dca3d73c1ce3c394aca6d8da4bf14c05604
issy <issy@issy.dev>
2024-12-01 11:49:38 +0000

    Add Kotlin support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,27 @@
 plugins {
-    id("java")
+  id("java")
+  kotlin("jvm")
 }
 
 group = "com.adventofcode"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    compileOnly("org.jetbrains:annotations:26.0.1")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.26.3")
+  compileOnly("org.jetbrains:annotations:26.0.1")
+  testImplementation(platform("org.junit:junit-bom:5.10.0"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation("org.assertj:assertj-core:3.26.3")
+  implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
+}
+
+kotlin {
+  jvmToolchain(23)
 }
